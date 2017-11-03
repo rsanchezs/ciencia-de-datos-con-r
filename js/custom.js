f36033a46ab5a50b5110f2d25b71c01535b01c0e
Rubén Sánchez Sancho <rsanchezs@uoc.edu>
2017-11-03 21:26:10 +0100

    modified function javascript

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,3 @@
-ELEMENT.classList.remove("powered-by-datacamp");
+var datacamp = document.getElementsByClassName("powered-by-datacamp");
+while (datacamp.length)
+    datacamp[0].classList.remove("powered-by-datacamp");
