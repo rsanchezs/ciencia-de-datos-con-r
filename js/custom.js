4d78f44c017aae5fb58d84f7b146e55fab287d1b
Rubén Sánchez Sancho <rsanchezs@uoc.edu>
2017-11-03 22:39:49 +0100

    JQuery Style

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,1 @@
-var datacamp = document.getElementsByClassName("powered-by-datacamp");
-while (datacamp.length > 0)
-    datacamp[0].parentNode.removeChild(datacamp[0]);
+$(".powered-by-datacamp").remove();
