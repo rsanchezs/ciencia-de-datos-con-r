da842385e8dca168b3c3dbe82aa4a503d97269cf
Rubén Sánchez Sancho <rsanchezs@uoc.edu>
2017-11-04 13:57:51 +0100

    Fixing error in custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,4 @@
-var parent_node = document.getElementById("normal markdown-section");
-var child_node = document.getElementById("powered-by-datacamp");
-parent_node.removeChild(child_node);
-
-
-
+var node = document.getElementById("powered-by-datacamp");
+if (node.parentNode) {
+  node.parentNode.removeChild(node);
+}
